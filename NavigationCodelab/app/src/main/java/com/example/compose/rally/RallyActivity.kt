644cf041ebf87bc9a9b92cd330f9d40f9f8032c3
Nguyen Ba Hung <hung@bio-rithm.com>
2025-09-12 14:24:48 +0700

    Enable deep links support

--- NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ NavigationCodelab/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -100,8 +100,9 @@ fun RallyApp() {
                     BillsScreen()
                 }
                 composable(
-                    route = "${SingleAccount.route}/{${SingleAccount.accountTypeArg}}",
+                    route = SingleAccount.routeWithArgs,
                     arguments = SingleAccount.arguments,
+                    deepLinks = SingleAccount.deepLinks,
                 ) { navBackStackEntry ->
                     val accountType =
                         navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
