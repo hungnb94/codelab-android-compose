ea4263b7afa568fe4e493efb71f0afe3e4902bb8
Hung Nguyen <hungnb94@gmail.com>
2025-09-13 15:51:32 +0700

    derivedStateOf

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/base/ExploreSection.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -36,10 +37,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.samples.crane.R
 import androidx.compose.samples.crane.data.ExploreModel
 import androidx.compose.samples.crane.home.OnExploreItemClicked
@@ -56,26 +62,50 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest.Builder
-
+import kotlinx.coroutines.launch
 
 @Composable
 fun ExploreSection(
     modifier: Modifier = Modifier,
     title: String,
     exploreList: List<ExploreModel>,
-    onItemClicked: OnExploreItemClicked
+    onItemClicked: OnExploreItemClicked,
 ) {
     Surface(modifier = modifier.fillMaxSize(), color = Color.White, shape = BottomSheetShape) {
         Column(modifier = Modifier.padding(start = 24.dp, top = 20.dp, end = 24.dp)) {
             Text(
                 text = title,
-                style = MaterialTheme.typography.caption.copy(color = crane_caption)
+                style = MaterialTheme.typography.caption.copy(color = crane_caption),
             )
             Spacer(Modifier.height(8.dp))
-            // TODO Codelab: derivedStateOf step
-            // TODO: Show "Scroll to top" button when the first item of the list is not visible
-            val listState = rememberLazyListState()
-            ExploreList(exploreList, onItemClicked, listState = listState)
+            Box(Modifier.weight(1f)) {
+                val listState = rememberLazyListState()
+                ExploreList(exploreList, onItemClicked, listState = listState)
+
+                val showButton by remember {
+                    derivedStateOf {
+                        listState.firstVisibleItemIndex > 0
+                    }
+                }
+                if (showButton) {
+                    val coroutineScope = rememberCoroutineScope()
+                    FloatingActionButton(
+                        backgroundColor = MaterialTheme.colors.primary,
+                        modifier =
+                            Modifier
+                                .align(Alignment.BottomEnd)
+                                .navigationBarsPadding()
+                                .padding(bottom = 8.dp),
+                        onClick = {
+                            coroutineScope.launch {
+                                listState.scrollToItem(0)
+                            }
+                        },
+                    ) {
+                        Text("Up!")
+                    }
+                }
+            }
         }
     }
 }
@@ -85,19 +115,19 @@ private fun ExploreList(
     exploreList: List<ExploreModel>,
     onItemClicked: OnExploreItemClicked,
     modifier: Modifier = Modifier,
-    listState: LazyListState = rememberLazyListState()
+    listState: LazyListState = rememberLazyListState(),
 ) {
     LazyColumn(
         modifier = modifier,
         contentPadding = WindowInsets.navigationBars.asPaddingValues(),
-        state = listState
+        state = listState,
     ) {
         items(exploreList) { exploreItem ->
             Column(Modifier.fillParentMaxWidth()) {
                 ExploreItem(
                     modifier = Modifier.fillParentMaxWidth(),
                     item = exploreItem,
-                    onItemClicked = onItemClicked
+                    onItemClicked = onItemClicked,
                 )
                 Divider(color = crane_divider_color)
             }
@@ -109,21 +139,24 @@ private fun ExploreList(
 private fun ExploreItem(
     modifier: Modifier = Modifier,
     item: ExploreModel,
-    onItemClicked: OnExploreItemClicked
+    onItemClicked: OnExploreItemClicked,
 ) {
     Row(
-        modifier = modifier
-            .clickable { onItemClicked(item) }
-            .padding(top = 12.dp, bottom = 12.dp)
+        modifier =
+            modifier
+                .clickable { onItemClicked(item) }
+                .padding(top = 12.dp, bottom = 12.dp),
     ) {
         ExploreImageContainer {
             Box {
-                val painter = rememberAsyncImagePainter(
-                    model = Builder(LocalContext.current)
-                        .data(item.imageUrl)
-                        .crossfade(true)
-                        .build()
-                )
+                val painter =
+                    rememberAsyncImagePainter(
+                        model =
+                            Builder(LocalContext.current)
+                                .data(item.imageUrl)
+                                .crossfade(true)
+                                .build(),
+                    )
                 Image(
                     painter = painter,
                     contentDescription = null,
@@ -135,9 +168,10 @@ private fun ExploreItem(
                     Image(
                         painter = painterResource(id = R.drawable.ic_crane_logo),
                         contentDescription = null,
-                        modifier = Modifier
-                            .size(36.dp)
-                            .align(Alignment.Center),
+                        modifier =
+                            Modifier
+                                .size(36.dp)
+                                .align(Alignment.Center),
                     )
                 }
             }
@@ -146,12 +180,12 @@ private fun ExploreItem(
         Column {
             Text(
                 text = item.city.nameToDisplay,
-                style = MaterialTheme.typography.h6
+                style = MaterialTheme.typography.h6,
             )
             Spacer(Modifier.height(8.dp))
             Text(
                 text = item.description,
-                style = MaterialTheme.typography.caption.copy(color = crane_caption)
+                style = MaterialTheme.typography.caption.copy(color = crane_caption),
             )
         }
     }
